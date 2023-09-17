3a8e42f475c8ebc475a5aa9eac061c01b8b238d1
marianonavarrete <marianonavarrete@192.168.1.8>
2023-09-17 16:41:54 -0500

    Add Person

---
+++ src/com/mant/pojo/Person.java
@@ -0,0 +1,20 @@
+package com.mant.pojo;
+
+public class Person {
+	private String name;
+	private String CI;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCI() {
+		return CI;
+	}
+	public void setCI(String cI) {
+		CI = cI;
+	}
+	
+	
+}
