7a4036e411c22a06e4acd89f38fd7a6083ed37d3
marianonavarrete <marianonavarrete@192.168.1.8>
2023-09-17 17:21:44 -0500

    Initializing person

--- src/com/mant/App.java
+++ src/com/mant/App.java
@@ -1,7 +1,11 @@
 package com.mant;
 
+import com.mant.pojo.Person;
+
 public class App {
 	public static void main(String[] args) {
+		Person person1 = new Person();
+		System.out.println(person1.getCI());
 		System.out.println("Initializing project");
 	}
 }
