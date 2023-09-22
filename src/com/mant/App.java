c48ab4f09f757246e5c80d7b2759b9135e6be63f
marianonavarrete <marianonavarrete@localhost>
2023-09-21 19:17:46 -0500

    adding changes

--- src/com/mant/App.java
+++ src/com/mant/App.java
@@ -4,9 +4,10 @@ import com.mant.pojo.Person;
 
 public class App {
 	public static void main(String[] args) {
+		System.out.println("this can be a problem");
 		Person person1 = new Person();
 		System.out.println(person1.getCI());
 		System.out.println("Initializing project");
-		System.out.println("Adding change from GitHub");
+		System.out.println("This change is from GitHub");
 	}
 }
